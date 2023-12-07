09a58b202049e11cf1cb9ad7e6238a45c8205091
leochausse <leochausse10@gmail.com>
2023-12-07 15:49:18 +0000

    Trabalhos em aula

---
+++ Leonardo Chaussê/exercicio2.py
@@ -0,0 +1,11 @@
+#resolução eu
+horas = input("quantas horas trabalha por dia?:")
+valor = input("qual o valor que recebe por hora trabalhada?:")
+total = float(horas) * float(valor) * 30
+print ("voce ira receber", total ,"€""por dia!")
+
+#resolução professor 
+nhours = input("number of hours:")
+nrate = input("Rate amount:")
+ntotal = float(nhours) * float(nrate)
+print("ior amount is:", ntotal,)
