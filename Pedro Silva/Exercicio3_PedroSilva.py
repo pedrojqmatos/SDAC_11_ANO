c7ff4ff2e7f430ba4307a103751cace28e015510
pedronogsilva <pedronsilva100@gmail.com>
2023-12-13 10:06:22 +0000

    Trabalhos em aula

--- Pedro Silva/Exercicio3_PedroSilva.py
+++ Pedro Silva/Exercicio3_PedroSilva.py
@@ -1,14 +1,15 @@
 
 hours = input("Enter Hours:")   #pergunta qual a quantidade horas
-rate = input("Enter Rate:")    #pergunta qual a quantidade de dinheiro por hora
-float(hours) 
-float(rate)    
+rates = input("Enter Rate:")    #pergunta qual a quantidade de dinheiro por hora
+hour = float(hours) 
+rate = float(rates)    
 
-if hours > 40: 
-    difhours = hours - 40 
-    mon = hours * rate
-    pay = (rate*1.5)*difhours + mon
-    print("O valor a ser pago é ", pay, "€")
+if hour > 40:
+    mon = hour * rate 
+    difhour = hour - 40
+    pay = (rate*1.5) * difhour
+    total = pay + mon
+    print("O valor a ser pago é ", total, "€")
 else:
-    mon = hours * rate
+    mon = hour * rate
     print("O valor a ser pago é ", mon, "€")
