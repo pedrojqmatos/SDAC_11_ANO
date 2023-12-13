f99bcc006f608aedb2e4c618120d9192f47bdb76
Martim <153207822+Martim-07@users.noreply.github.com>
2023-12-13 07:44:11 +0000

    ex3

--- Martim Costa/Martim ex3.py
+++ Martim Costa/Martim ex3.py
@@ -4,7 +4,7 @@ if float(hour)>40:
     fhour=float(hour)
     frate=float(rate)
     hourt=fhour*frate+(fhour-40)*1.5*frate
-    print("payment is: ",hourt)
+    print(("you have worked a total of"),fhour,("Hrs and your payment is: "),hourt)
 else:
      nhour=float(hour)*float(rate)
      print("payment is:",nhour)
