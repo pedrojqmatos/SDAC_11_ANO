2423803598f2ad583f5f410853131bbcd1e4c094
pedronogsilva <pedronsilva100@gmail.com>
2023-12-14 23:32:23 +0000

    Trabalho em aula

--- Pedro Silva/Exercício2_PedroSilva.py
+++ Pedro Silva/Exercício2_PedroSilva.py
@@ -1,4 +1,3 @@
-
 a = input("Enter Hours:")   #pergunta qual a quantidade horas
 b = input("Enter Rate:")    #pergunta qual a quantidade de dinheiro por hora
 pay = float(a) * float(b)     #cria uma nova váriavel e pega nas anteriores e definindo-as como float e por fim se mutiplica
