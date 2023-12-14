c123ef633dcafded5cf6dd58991b4fca34a16b86
Martim <153207822+Martim-07@users.noreply.github.com>
2023-12-14 14:54:18 +0000

    ex5

---
+++ Martim Costa/Martim ex5.py
@@ -0,0 +1,15 @@
+hour=input ("Enter hours: ")
+rate=input ("Enter rate: ")
+fhour=float(hour)
+frate=float(rate)
+def computepay (hour,rate):
+    if fhour> 40:
+        payx=frate*fhour+(fhour-40)*1.5*frate
+        return payx
+    else:
+        payy=frate*fhour   
+        return payy
+final=computepay(hour,rate)
+print(final)
+
+
