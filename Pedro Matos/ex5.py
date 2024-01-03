1a836c6e39695843600cc33ac734b464de81a055
Pedro Matos <pedrojqmatos@gmail.com>
2024-01-03 15:17:36 +0000

    Correção dos exercicíos em aula

---
+++ Pedro Matos/ex5.py
@@ -0,0 +1,20 @@
+#gross pay program
+
+def computepay(hours, rate):
+    print("Compute pay is: ", hours, rate)
+    if hours > 40:
+        reg = rate * hours
+        otp = (hours - 40) * (rate * 1.5)
+        pay = reg + otp
+    else:
+        pay = hours * rate
+    print("Returning: ", pay)
+    return pay
+
+nhours = input("Number of hours:")
+nrate = input("Rate amount:")
+fhours = float(nhours)
+frate = float(nrate)
+
+xp = computepay(fhours, frate)
+print("Pay: ", xp)
