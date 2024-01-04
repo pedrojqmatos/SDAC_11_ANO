956a8be42a56387ed8572e051cfd3ca9ebc9e59d
pedronogsilva <pedronsilva100@gmail.com>
2024-01-04 09:53:11 +0000

    Trabalho em aula

--- Pedro Silva/Exercício5_PedroSilva.py
+++ Pedro Silva/Exercício5_PedroSilva.py
@@ -1,7 +1,3 @@
-hours = input("Enter Hours:")   #pergunta qual a quantidade horas
-rates = input("Enter Rate:")    #pergunta qual a quantidade de dinheiro por hora
-hour = float(hours)
-rate = float(rates)
 
 def computepay(hour, rate):
 
@@ -10,10 +6,15 @@ def computepay(hour, rate):
         difhour = hour - 40
         pay = (rate*1.5) * difhour
         total = pay + mon
-        print("Vais receber:", total)
 
     else:
-        mon = hour * rate
-        print("Vais receber:", mon)
+        total = hour * rate
+    return total
 
-computepay(hour, rate)
+hours = input("Enter Hours:")
+rates = input("Enter Rate:")    
+fhour = float(hours)
+frate = float(rates)
+ 
+xp = computepay(fhour, frate)
+print("Pay:", xp)
