3df519b7ab4635901be53f4545af90e24158c367
Pedro Silva <pedronsilva100@gmail.com>
2024-01-14 23:03:45 +0000

    Trabalho em aula

--- Pedro Silva/Exercício7_PedroSilva.py
+++ Pedro Silva/Exercício7_PedroSilva.py
@@ -9,13 +9,13 @@ valor = float(escreve_valor)
 print(valor)
 
 round = str(valor)
-num = round[0:4]
+num = round[0:5]
 #escreve o numero ate a posição 3 
-num5 = round[0:3]
+num5 = round[0:4]
 #escreve o numero ate a posição 2
-numposion3 = num[3:]
+numposion3 = num[4:]
 #escreve o valor so da posição 3
-numposion2 = num[2:3]
+numposion2 = num[3:4]
 #escreve o valor so da posição 2
 
 if numposion3 < "5":
@@ -23,6 +23,6 @@ if numposion3 < "5":
     #se o valor na 3 posição for menor que 5 returna o valor ate a 3 casa sem alteração
 elif numposion3 >= "5":
     numup5float = float(num5)
-    finaly = numup5float + 0.1
+    finaly = numup5float + 0.01
     print(finaly)
     #se o valor na 3 posição for maior ou igual a 5 retorna o valor com alterações
