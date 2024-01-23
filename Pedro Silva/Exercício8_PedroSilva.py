7df7ec14d02c42ad1d5af8df04df4f972eadfd69
Pedro Silva <pedronsilva100@gmail.com>
2024-01-23 21:57:29 +0000

    Trabalho em aula

--- Pedro Silva/Exercício8_PedroSilva.py
+++ Pedro Silva/Exercício8_PedroSilva.py
@@ -2,7 +2,7 @@ ffile = input("Open desegnated file:")
 try:
     fopen = open(ffile)
 except:
-    print("Invalid file name". ffile, "cannot be opened")
+    print("Invalid file name", ffile, "cannot be opened")
     quit()
 #se o nome corresponder abre, se nao nao abre nada e fecha
 for line in fopen:
