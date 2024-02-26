018707387797b5689cf66ee2b1423d880d6e34a1
Martim <153207822+Martim-07@users.noreply.github.com>
2024-02-26 11:47:02 +0000

    ex de 9 ate 13

---
+++ Martim Costa/Martim ex9/ex9_debbug_05.py
@@ -0,0 +1,14 @@
+fopen = open('mbox-short.txt')
+
+for line in fopen :
+    line = line.rstrip()
+    print('Line:', line)
+    if line == '' :
+        print('Skipped Blank Line')
+        continue
+    words = line.split()
+    print('Words', words)
+    if words[0] != 'From' :
+        print('Ignored')
+        continue
+    print(words[2])
