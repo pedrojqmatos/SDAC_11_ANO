1b3cd3139cb03696908e56c9f05208bed2ee1cb4
Breno Silva <brenobatista108@gmail.com>
2024-03-05 15:25:50 +0000

    .

---
+++ Breno Silva/Ex11/Ex11.py
@@ -0,0 +1,19 @@
+fname = input('Enter file: ')
+if len(fname) < 1 : fname = 'clown.txt'
+handle = open(fname)
+
+new_dictionary = dict()
+for line in handle :
+    line = line.rstrip()
+    words = line.split()
+    for word in words :
+        new_dictionary[word] = new_dictionary.get(word, 0) + 1
+   
+temp = list()
+for k, v in new_dictionary.items() :
+    temp.append((v,k))
+    
+temp = sorted(temp, reverse=True)
+
+for v,k in temp[:5] :
+    print(k,v)
