0c21a87290965743eb903dbacf5971d1c37e012d
MargaridaAlves11 <margaridaalvesperdigao@gmail.com>
2024-03-06 12:09:03 +0000

    EX

---
+++ Margarida Alves/EX 5/ex5.py
@@ -0,0 +1,18 @@
+def computepay(hours, rate):
+    print("Compute pay is: ", hours, rate)
+    if hours > 40:
+        reg = rate * hours
+        otp = (hours - 40) * (rate * 1.5)
+        pay = reg + otp
+    else:
+        pay = hours * rate
+    print("Returning: ", pay)
+    return pay
+
+nhours = input("Number of hours:")
+nrate = input("Rate amount:")
+fhours = float(nhours)
+frate = float(nrate)
+
+xp = computepay(fhours, frate)
+print("Pay: ", xp)
---
+++ Margarida Alves/EX 7/ex7.py
@@ -0,0 +1,10 @@
+str = "X-DSPAM-Confidence: 0.8475 "
+
+cpos = str.find(":")
+print("Colon position is:", cpos)
+npos = str[cpos + 1 :].strip()
+print("Value after the colon is:", npos)
+fvalue = float(npos)
+print("Floating value is:", fvalue)
+sum = fvalue * 12
+print("The sum result is", sum, "The round value is", round(sum))
